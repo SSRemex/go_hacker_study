db3085fe4039402ed28acb1ca1c3072acca5cf93
SSRemex <769885884@qq.com>
2022-09-12 23:58:09 +0800

    shodan api

---
+++ 02-http-api/01-shodan-client/shodan/api.go
@@ -0,0 +1,31 @@
+package shodan
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type APIInfo struct {
+	QueryCredits int    `json:"query_credits"`
+	ScanCredits  int    `json:"scan_credits"`
+	Telnet       bool   `json:"telnet"`
+	Plan         string `json:"plan"`
+	HTTPS        bool   `json:"https"`
+	Unlocked     bool   `json:"unlocked"`
+}
+
+func (client *Client) APIInfo() (*APIInfo, error) {
+	res, err := http.Get(fmt.Sprintf("%s/api_info?key=%s", BaseURL, client.apiKey))
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	var ret APIInfo
+	if err := json.NewDecoder(res.Body).Decode(&ret); err != nil {
+		return nil, err
+	}
+	return &ret, nil
+
+}
---
+++ 02-http-api/01-shodan-client/shodan/go.mod
@@ -0,0 +1,9 @@
+module shodan
+
+require (
+	shodan v0.0.0
+)
+replace (
+	"shodan" => "../../shodan"
+)
+go 1.19
